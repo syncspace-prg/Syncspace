086c5bf0d2e3a45621ef87d7b1500f743f4683a4
syncspace-prg <harrisen.wang@temposchool.org>
2026-04-07 21:34:04 -0600

    Update app.js

--- app.js
+++ app.js
@@ -3,13 +3,10 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebas
 const firebaseConfig = {
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBuqFPs5qfenyzPo5mcO8xhVaBYCTb5Cfw",
-  authDomain: "syncspace-b0c77.firebaseapp.com",
-  projectId: "syncspace-b0c77",
-  storageBucket: "syncspace-b0c77.firebasestorage.app",
-  messagingSenderId: "898472105255",
-  appId: "1:898472105255:web:92fe09babcb6a9da30c502",
-  measurementId: "G-1S5YC0TJES"
+  const firebaseConfig = {
+  apiKey: "test",
+  authDomain: "test",
+  projectId: "test"
 };
 
 const app = initializeApp(firebaseConfig);
