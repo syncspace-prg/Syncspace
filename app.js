5d4bcc11ab6c1c1348cf05fb37c5b051143f147f
syncspace-prg <harrisen.wang@temposchool.org>
2026-04-10 16:18:41 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,35 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+
 window.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("status").textContent = "JS WORKS ✅";
+  const status = document.getElementById("status");
+  const input = document.getElementById("messageInput");
+
+  status.textContent = "JS LOADED ✔️";
+
+  const supabaseUrl ="https://fizuvliegegrsoqdjjfd.supabase.co";
+  const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZpenV2bGllZ2VncnNvcWRqamZkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU3ODQzNTksImV4cCI6MjA5MTM2MDM1OX0.XUDBmYuEYBVr3QgnZB0dMs6sSuT7UPOrUTP5BhnbMew";
+
+  const supabase = createClient(supabaseUrl, supabaseKey);
+
+  document.getElementById("sendBtn").onclick = async () => {
+    const text = input.value;
+
+    if (!text) {
+      status.textContent = "Type something first";
+      return;
+    }
+
+    status.textContent = "Sending...";
+
+    const { error } = await supabase.from("messages").insert({
+      text
+    });
+
+    if (error) {
+      status.textContent = "ERROR: " + error.message;
+    } else {
+      status.textContent = "SENT ✅";
+      input.value = "";
+    }
+  };
 });
