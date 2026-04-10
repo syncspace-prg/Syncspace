99e8b01b8f1dfd44e929d8477935828bdbccb349
syncspace-prg <harrisen.wang@temposchool.org>
2026-04-09 19:41:30 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
 // 🔴 PUT YOUR REAL VALUES HERE
-const supabaseUrl = "https://YOURPROJECT.supabase.co";
+const supabaseUrl = https://fizuvliegegrsoqdjjfd.supabase.co
 const supabaseKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZpenV2bGllZ2VncnNvcWRqamZkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU3ODQzNTksImV4cCI6MjA5MTM2MDM1OX0.XUDBmYuEYBVr3QgnZB0dMs6sSuT7UPOrUTP5BhnbMew
 
 const supabase = createClient(supabaseUrl, supabaseKey);
