7c250f2345c32ad1bef81ef81708d2ded99dd16e
syncspace-prg <harrisen.wang@temposchool.org>
2026-04-10 16:14:46 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 window.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("status").textContent = "JS IS RUNNING ✅";
+  document.getElementById("status").textContent = "JS WORKS ✅";
 });
