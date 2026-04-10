06d7cc0aa09e6e3b786cde4379d497a59c7a3c46
syncspace-prg <harrisen.wang@temposchool.org>
2026-04-10 16:03:16 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,35 +1,3 @@
-pimport { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
-
-const supabaseUrl = "https://fizuvliegegrsoqdjjfd.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZpenV2bGllZ2VncnNvcWRqamZkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU3ODQzNTksImV4cCI6MjA5MTM2MDM1OX0.XUDBmYuEYBVr3QgnZB0dMs6sSuT7UPOrUTP5BhnbMew";
-
-const supabase = createClient(supabaseUrl, supabaseKey);
-
 window.addEventListener("DOMContentLoaded", () => {
-  const status = document.getElementById("status");
-  const chatBox = document.getElementById("chatBox");
-  const input = document.getElementById("messageInput");
-
-  status.textContent = "JS LOADED ✔️";
-
-  document.getElementById("sendBtn").onclick = async () => {
-    const text = input.value;
-
-    if (!text) {
-      status.textContent = "Type something first";
-      return;
-    }
-
-    const { error } = await supabase.from("messages").insert({
-      text
-    });
-
-    if (error) {
-      status.textContent = "ERROR: " + error.message;
-      return;
-    }
-
-    status.textContent = "SENT ✅";
-    input.value = "";
-  };
+  document.getElementById("status").textContent = "JS IS RUNNING ✅";
 });
