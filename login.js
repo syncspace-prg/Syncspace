71306c238f5e1859a20779647cc61b9a7972e291
syncspace-prg <harrisen.wang@temposchool.org>
2026-04-13 17:21:32 -0600

    Update login.js

--- login.js
+++ login.js
@@ -1,6 +1,6 @@
 const client = window.supabase.createClient(
-  "YOUR_SUPABASE_URL",
-  "YOUR_SUPABASE_ANON_KEY"
+  "https://fizuvliegegrsoqdjjfd.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZpenV2bGllZ2VncnNvcWRqamZkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU3ODQzNTksImV4cCI6MjA5MTM2MDM1OX0.XUDBmYuEYBVr3QgnZB0dMs6sSuT7UPOrUTP5BhnbMew"
 );
 
 const status = document.getElementById("status");
