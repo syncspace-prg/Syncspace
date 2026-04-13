26822b5166033cd825e6372cc374af1e54e5bae6
syncspace-prg <harrisen.wang@temposchool.org>
2026-04-13 17:24:52 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
-const status = document.getElementById("status");
+const client = window.supabase.createClient(
+  "https://fizuvliegegrsoqdjjfd.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZpenV2bGllZ2VncnNvcWRqamZkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU3ODQzNTksImV4cCI6MjA5MTM2MDM1OX0.XUDBmYuEYBVr3QgnZB0dMs6sSuT7UPOrUTP5BhnbMew"
+);
+
+client.auth.getUser().then(({ data }) => {
+  if (!data.user) {
+    window.location.href = "login.html";
+  }
+});const status = document.getElementById("status");
 const chatBox = document.getElementById("chatBox");
 const input = document.getElementById("messageInput");
 const btn = document.getElementById("sendBtn");
